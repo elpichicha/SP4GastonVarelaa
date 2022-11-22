97fc13b5cfe62b65a83787c2b0c5e098731db6d4
Varela <Varela@DESKTOP-L4N34KB>
2022-11-22 00:00:53 -0300

    Agregar archivos de proyecto.

---
+++ SP4GastonVarelaa/frmMain.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace SP4GastonVarelaa
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTotalFinal = new System.Windows.Forms.TextBox();
+            this.txtPostres = new System.Windows.Forms.TextBox();
+            this.txtBebidasCA = new System.Windows.Forms.TextBox();
+            this.txtBebidasSA = new System.Windows.Forms.TextBox();
+            this.txtComidas = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtMozo = new System.Windows.Forms.TextBox();
+            this.lblTotalFinal = new System.Windows.Forms.Label();
+            this.lblPostres = new System.Windows.Forms.Label();
+            this.lblBebidaCA = new System.Windows.Forms.Label();
+            this.lblBebidaSA = new System.Windows.Forms.Label();
+            this.lblComidas = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblMozo = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnMozoDelDia = new System.Windows.Forms.Button();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.dgvTabla = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTotalFinal
+            // 
+            this.txtTotalFinal.Location = new System.Drawing.Point(558, 365);
+            this.txtTotalFinal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotalFinal.Name = "txtTotalFinal";
+            this.txtTotalFinal.ReadOnly = true;
+            this.txtTotalFinal.Size = new System.Drawing.Size(74, 20);
+            this.txtTotalFinal.TabIndex = 37;
+            // 
+            // txtPostres
+            // 
+            this.txtPostres.Location = new System.Drawing.Point(481, 364);
+            this.txtPostres.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPostres.Name = "txtPostres";
+            this.txtPostres.ReadOnly = true;
+            this.txtPostres.Size = new System.Drawing.Size(67, 20);
+            this.txtPostres.TabIndex = 36;
+            // 
+            // txtBebidasCA
+            // 
+            this.txtBebidasCA.Location = new System.Drawing.Point(364, 365);
+            this.txtBebidasCA.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBebidasCA.Name = "txtBebidasCA";
+            this.txtBebidasCA.ReadOnly = true;
+            this.txtBebidasCA.Size = new System.Drawing.Size(106, 20);
+            this.txtBebidasCA.TabIndex = 35;
+            // 
+            // txtBebidasSA
+            // 
+            this.txtBebidasSA.Location = new System.Drawing.Point(257, 365);
+            this.txtBebidasSA.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBebidasSA.Name = "txtBebidasSA";
+            this.txtBebidasSA.ReadOnly = true;
+            this.txtBebidasSA.Size = new System.Drawing.Size(93, 20);
+            this.txtBebidasSA.TabIndex = 34;
+            // 
+            // txtComidas
+            // 
+            this.txtComidas.Location = new System.Drawing.Point(172, 364);
+            this.txtComidas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtComidas.Name = "txtComidas";
+            this.txtComidas.ReadOnly = true;
+            this.txtComidas.Size = new System.Drawing.Size(74, 20);
+            this.txtComidas.TabIndex = 33;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(295, 292);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(89, 20);
+            this.txtTotal.TabIndex = 32;
+            // 
+            // txtMozo
+            // 
+            this.txtMozo.Location = new System.Drawing.Point(172, 292);
+            this.txtMozo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMozo.Name = "txtMozo";
+            this.txtMozo.ReadOnly = true;
+            this.txtMozo.Size = new System.Drawing.Size(100, 20);
+            this.txtMozo.TabIndex = 31;
+            // 
+            // lblTotalFinal
+            // 
+            this.lblTotalFinal.AutoSize = true;
+            this.lblTotalFinal.Location = new System.Drawing.Point(556, 348);
+            this.lblTotalFinal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotalFinal.Name = "lblTotalFinal";
+            this.lblTotalFinal.Size = new System.Drawing.Size(56, 13);
+            this.lblTotalFinal.TabIndex = 30;
+            this.lblTotalFinal.Text = "Total Final";
+            // 
+            // lblPostres
+            // 
+            this.lblPostres.AutoSize = true;
+            this.lblPostres.Location = new System.Drawing.Point(479, 348);
+            this.lblPostres.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPostres.Name = "lblPostres";
+            this.lblPostres.Size = new System.Drawing.Size(42, 13);
+            this.lblPostres.TabIndex = 29;
+            this.lblPostres.Text = "Postres";
+            // 
+            // lblBebidaCA
+            // 
+            this.lblBebidaCA.AutoSize = true;
+            this.lblBebidaCA.Location = new System.Drawing.Point(362, 348);
+            this.lblBebidaCA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBebidaCA.Name = "lblBebidaCA";
+            this.lblBebidaCA.Size = new System.Drawing.Size(104, 13);
+            this.lblBebidaCA.TabIndex = 28;
+            this.lblBebidaCA.Text = "Bebidas con Alcohol";
+            // 
+            // lblBebidaSA
+            // 
+            this.lblBebidaSA.AutoSize = true;
+            this.lblBebidaSA.Location = new System.Drawing.Point(255, 348);
+            this.lblBebidaSA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBebidaSA.Name = "lblBebidaSA";
+            this.lblBebidaSA.Size = new System.Drawing.Size(99, 13);
+            this.lblBebidaSA.TabIndex = 27;
+            this.lblBebidaSA.Text = "Bebidas sin Alcohol";
+            // 
+            // lblComidas
+            // 
+            this.lblComidas.AutoSize = true;
+            this.lblComidas.Location = new System.Drawing.Point(177, 348);
+            this.lblComidas.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblComidas.Name = "lblComidas";
+            this.lblComidas.Size = new System.Drawing.Size(47, 13);
+            this.lblComidas.TabIndex = 26;
+            this.lblComidas.Text = "Comidas";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(293, 276);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 25;
+            this.lblTotal.Text = "Total";
+            // 
+            // lblMozo
+            // 
+            this.lblMozo.AutoSize = true;
+            this.lblMozo.Location = new System.Drawing.Point(177, 276);
+            this.lblMozo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMozo.Name = "lblMozo";
+            this.lblMozo.Size = new System.Drawing.Size(33, 13);
+            this.lblMozo.TabIndex = 24;
+            this.lblMozo.Text = "Mozo";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(643, 397);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(88, 25);
+            this.btnSalir.TabIndex = 23;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Enabled = false;
+            this.btnTotal.Location = new System.Drawing.Point(68, 350);
+            this.btnTotal.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(87, 32);
+            this.btnTotal.TabIndex = 22;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            // 
+            // btnMozoDelDia
+            // 
+            this.btnMozoDelDia.Enabled = false;
+            this.btnMozoDelDia.Location = new System.Drawing.Point(68, 282);
+            this.btnMozoDelDia.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMozoDelDia.Name = "btnMozoDelDia";
+            this.btnMozoDelDia.Size = new System.Drawing.Size(87, 31);
+            this.btnMozoDelDia.TabIndex = 21;
+            this.btnMozoDelDia.Text = "Mozo del Dia";
+            this.btnMozoDelDia.UseVisualStyleBackColor = true;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(623, 28);
+            this.btnValidar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(109, 37);
+            this.btnValidar.TabIndex = 20;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            // 
+            // dgvTabla
+            // 
+            this.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgvTabla.Location = new System.Drawing.Point(68, 28);
+            this.dgvTabla.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvTabla.Name = "dgvTabla";
+            this.dgvTabla.RowHeadersWidth = 51;
+            this.dgvTabla.RowTemplate.Height = 24;
+            this.dgvTabla.Size = new System.Drawing.Size(517, 223);
+            this.dgvTabla.TabIndex = 19;
+            this.dgvTabla.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTabla_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mozo";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 90;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Comidas";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column2.Width = 90;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Bebidas sin Alcohol";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column3.Width = 90;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Bebidas con Alcohol";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column4.Width = 90;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Postres";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column5.Width = 90;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SP4GastonVarelaa.Properties.Resources.gettyimages_157510804_612x612;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(810, 459);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 448);
+            this.Controls.Add(this.txtTotalFinal);
+            this.Controls.Add(this.txtPostres);
+            this.Controls.Add(this.txtBebidasCA);
+            this.Controls.Add(this.txtBebidasSA);
+            this.Controls.Add(this.txtComidas);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtMozo);
+            this.Controls.Add(this.lblTotalFinal);
+            this.Controls.Add(this.lblPostres);
+            this.Controls.Add(this.lblBebidaCA);
+            this.Controls.Add(this.lblBebidaSA);
+            this.Controls.Add(this.lblComidas);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblMozo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnMozoDelDia);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.dgvTabla);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmMain";
+            this.Text = "LaMilanga";
+            this.Load += new System.EventHandler(this.frmMain_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabla)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTotalFinal;
+        private System.Windows.Forms.TextBox txtPostres;
+        private System.Windows.Forms.TextBox txtBebidasCA;
+        private System.Windows.Forms.TextBox txtBebidasSA;
+        private System.Windows.Forms.TextBox txtComidas;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtMozo;
+        private System.Windows.Forms.Label lblTotalFinal;
+        private System.Windows.Forms.Label lblPostres;
+        private System.Windows.Forms.Label lblBebidaCA;
+        private System.Windows.Forms.Label lblBebidaSA;
+        private System.Windows.Forms.Label lblComidas;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblMozo;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnMozoDelDia;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.DataGridView dgvTabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SP4GastonVarelaa/frmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP4GastonVarelaa
+{
+    public partial class frmMain : Form
+    {
+        public int[] vecDatos = new int[10];
+        public int[,] matDatos = new int[5, 4];
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            for (i = 0; i <= 4; i++)
+            {
+                dgvTabla.Rows.Add();
+            }
+            //Asigno nombres a la primer columna
+            dgvTabla.Rows[0].Cells[0].Value = "Julio";
+            dgvTabla.Rows[1].Cells[0].Value = "Esteban";
+            dgvTabla.Rows[2].Cells[0].Value = "Javier";
+            dgvTabla.Rows[3].Cells[0].Value = "Gonzalo";
+            dgvTabla.Rows[4].Cells[0].Value = "Alberto";
+            //Todas las celdas de la columna cero "Mozo" son ineditables
+            dgvTabla.Columns[0].ReadOnly = true;
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMozoDelDia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvTabla_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
